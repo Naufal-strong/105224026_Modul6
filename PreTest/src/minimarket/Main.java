1d3828e315384cd75e258676dc46563131a73d7f
Naufal-strong <naufalfakhrianto20@gmail.com>
2026-04-07 15:09:35 +0700

    PreTest

---
+++ PreTest/src/minimarket/Main.java
@@ -0,0 +1,9 @@
+package minimarket;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MakananRingan mk1 = new MakananRingan("Pota Bee", 9000, "Rumput Laut");
+        mk1.tampilkanDetail();
+    }
+}
